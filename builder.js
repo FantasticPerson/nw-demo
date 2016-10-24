9a01b08bd42a6adbb4cca181d7e77e87a3022b8c
dandan.wu <dandan.wu@alo7.com>
2016-10-24 11:56:23 +0800

    update support for xp 添加flash 插件

--- builder.js
+++ builder.js
@@ -4,9 +4,14 @@
 var NwBuilder = require('nw-builder');
 
 var nw = new NwBuilder({
-    files: ['./package.json','./index.html'], // 包含文件
-    platforms: ['win64','win32','osx64'], // 打包的平台
-    version: '0.17.0' // 使用 NW.js 的版本
+    files: [
+        './package.json',
+        './index.html',
+        './PepperFlash/pepflashplayer.dll',
+        './PepperFlash/manifest.json'
+    ], // 包含文件
+    platforms: ['win32'], // 打包的平台
+    version: '0.14.7' // 使用 NW.js 的版本
 });
 
 nw.on('log', console.log); // 日志打印函数
