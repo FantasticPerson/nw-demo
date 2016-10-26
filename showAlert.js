69939c4ea64dc656f43f5cb1cd3aa17565130d2d
dandan.wu <dandan.wu@alo7.com>
2016-10-26 18:07:34 +0800

    fix alert 导致  app crash 的问题

--- showAlert.js
+++ showAlert.js
@@ -1,11 +1,22 @@
 function showAlert(msg=' '){
-	var date = Date.now();
-    var gui = nodeRequire('nw.gui');
-    var alert = gui.Window.open('http://10.10.61.104:8080/index.html?msg='+msg+'&data='+date,{fullscreen:false,always_on_top:true,resizable:false,frame:false,width:400,height:120});
+    if(window.hasOwnProperty('nodeRequire')) {
+        var date = Date.now();
+        var gui = nodeRequire('nw.gui');
+        gui.Window.open('http://10.10.61.104:8080/index.html?msg=' + msg + '&data=' + date, {
+            fullscreen: false,
+            always_on_top: true,
+            resizable: false,
+            frame: false,
+            width: 400,
+            height: 120
+        });
+    } else {
+        window.alert(msg);
+    }
 }
 
 function checkIsNodeEnviroment(){
-    if(indow.hasOwnProperty('require') && !window.hasOwnProperty('nodeRequire')){
+    if(window.hasOwnProperty('require') && !window.hasOwnProperty('nodeRequire')){
         window.nodeRequire = window.require;
         delete window.require;
     }
