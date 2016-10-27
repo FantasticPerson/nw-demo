2fa9f9b04cf229079b1753aac6d198b740d8f756
dandan.wu <dandan.wu@alo7.com>
2016-10-27 11:09:34 +0800

    fix alert 导致  app crash 的问题

--- builder.js
+++ builder.js
@@ -6,9 +6,7 @@ var NwBuilder = require('nw-builder');
 var nw = new NwBuilder({
     files: [
         './package.json',
-        './index.html',
-        './PepperFlash/pepflashplayer.dll',
-        './PepperFlash/manifest.json'
+        './index.html'
     ], // 包含文件
     platforms: ['win32'], // 打包的平台
     version: '0.14.7' // 使用 NW.js 的版本
--- showAlert.js
+++ showAlert.js
@@ -1,17 +1,21 @@
-function showAlert(msg=' '){
+function setAlertFunc(){
+    if(window.hasOwnProperty('require') && !window.hasOwnProperty('nodeRequire')){
+        window.nodeRequire = window.require;
+        delete window.require;
+    }
     if(window.hasOwnProperty('nodeRequire')) {
-        var date = Date.now();
-        var gui = nodeRequire('nw.gui');
-        gui.Window.open('http://10.10.61.104:8080/index.html?msg=' + msg + '&data=' + date, {
-            fullscreen: false,
-            always_on_top: true,
-            resizable: false,
-            frame: false,
-            width: 400,
-            height: 120
-        });
-    } else {
-        window.alert(msg);
+        window.alert = function(msg = ' ') {
+            var date = Date.now();
+            var gui = nodeRequire('nw.gui');
+            gui.Window.open('http://10.10.61.104:8080/index.html?msg=' + msg + '&data=' + date, {
+                fullscreen: false,
+                always_on_top: true,
+                resizable: false,
+                frame: false,
+                width: 400,
+                height: 120
+            });
+        }
     }
 }
 
